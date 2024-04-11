e7ce64c5402742ccd2ca1eb4d7c7cd9fb0cce9fc
yd170214 <sh20170214@gmail.com>
2024-04-11 23:19:06 +0900

    fix: empty Card component

--- src/components/rollingPost/card/Card.jsx
+++ src/components/rollingPost/card/Card.jsx
@@ -1,7 +1,9 @@
 import './card.scss';
+import '../../../styles/components.scss';
 import { Link } from 'react-router-dom';
 import RollingPostEdit from '../../../pages/rollingPostEdit/RollingPostEdit';
 import plusIcon from '../../../assets/icon/icon_plus.svg';
+import mockImg from './mock_img.png';
 import { useState } from 'react';
 
 function Card() {
@@ -9,20 +11,33 @@ function Card() {
 
   const handleCardClick = () => {
     console.log('버튼 클릭!');
+    setHasCard(true);
     return <Link to={<RollingPostEdit />} />;
   };
 
   return (
     <>
       {hasCard ? (
-        <button className="card" onClick={handleCardClick}>
-          <div className="card--empty">
-            <img src={plusIcon} alt="추가하기" />
+        <button className="card">
+          <div className="card--from-group">
+            <div className="card--from-group--title">
+              <img src={mockImg} alt="목업 이미지" />
+              {/* TODO : From 뒤에 이름은 받아서 와야 함 */}
+              <div className="">
+                <span>From.김동훈</span>
+                {/* 뱃지도 받아서 와야 함 */}
+                <span>동료</span>
+              </div>
+            </div>
           </div>
         </button>
       ) : (
-        <button>
-          <div></div>
+        <button className="card" onClick={handleCardClick}>
+          <div className="card--empty">
+            <div className="card--img-container">
+              <img src={plusIcon} alt="추가하기" />
+            </div>
+          </div>
         </button>
       )}
     </>
