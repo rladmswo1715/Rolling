1481d4f515742af5d2293a8b8372b8b8b78e3017
kimmiso <vmffjrm@naver.com>
2024-04-11 17:18:14 +0900

    Feat: layout 작업 하여 페이지에 적용, 일부 아이콘, 로고 이미지 추가

--- src/pages/rollingList/RollingList.jsx
+++ src/pages/rollingList/RollingList.jsx
@@ -1,9 +1,15 @@
-import './RollingList.css';
+import './rollingList.scss';
 
 export default function RollingList() {
   return (
-    <div>
-      롤링페이퍼 리스트 페이지
-    </div>
+    <section className="layout__list">
+      <div className="inner__size-full inner__body">
+        {/* 여기서 작업해주세요 */}
+        롤링페이퍼 리스트 페이지
+        <div>
+          박스
+        </div>
+      </div>
+    </section>
   );
 }
