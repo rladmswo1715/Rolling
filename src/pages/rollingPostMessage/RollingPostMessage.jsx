8f2b616507b2b399c817ce7cded04ea45186dfb8
eunjae <rladmswo1715@naver.com>
2024-04-11 17:12:12 +0900

    PR 템플릿 테스트

--- src/pages/rollingPostMessage/RollingPostMessage.jsx
+++ src/pages/rollingPostMessage/RollingPostMessage.jsx
@@ -1,5 +1,42 @@
 export default function RollingPostMessage() {
   return (
-    <section>롤링페이퍼에 메세지 보내기 페이지</section>
+    <section>
+      <form>
+        <div>
+          <span>From.</span>
+          <input type="text" />
+        </div>
+
+        <div>
+          <span>프로필 이미지</span>
+          <div>
+            이미지
+          </div>
+          <div>
+            <span>프로필 이미지를 선택해주세요!</span>
+            <div>이미지 리스트</div>
+          </div>
+        </div>
+
+        <div>
+          <span>상대와의 관계</span>
+          <select>
+            <option>지인</option>
+          </select>
+        </div>
+
+        <div>
+          <span>내용을 입력해 주세요</span>
+          <div>에디터</div>
+        </div>
+
+        <div>
+          <span>폰트 선택</span>
+          <select>
+            <option>Noto Sans</option>
+          </select>
+        </div>
+      </form>
+    </section>
   );
 }
