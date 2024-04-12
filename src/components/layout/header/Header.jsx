e042f7d2be03832fd83f36ded74dace3b7510497
Batrnan <tjdwo2323@naver.com>
2024-04-12 16:13:21 +0900

    Feat: 메인 페이지, 헤더 마크업

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,15 +1,19 @@
-import './header.scss';
-import logoImage from '../../../assets/logo/logo_rolling.svg'
+import "./header.scss";
+import logoImage from "../../../assets/logo/logo_rolling.svg";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className='header__wrap'>
-      <div className='inner__size-ls d__flex--btw header__inner'>
+    <header className="header__wrap">
+      <div className="inner__size-ls d__flex--btw header__inner">
         {/* 여기서 부터 작업해 주세요. */}
-        <img src={logoImage} alt="롤링" className='header__image__logo' />
-        <button>롤링</button>
+        <Link to={"/"}>
+          <img src={logoImage} alt="롤링" className="header__image__logo" />
+        </Link>
+        <Link to={"/post"}>
+          <button className="button">롤링 페이퍼 만들기</button>
+        </Link>
       </div>
     </header>
   );
 }
-
