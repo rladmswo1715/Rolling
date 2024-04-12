dc43b071c8ac024d3313195ce227327589876566
yd170214 <sh20170214@gmail.com>
2024-04-12 09:51:52 +0900

    Docs: create Card components

--- src/components/rollingPost/card/Card.jsx
+++ src/components/rollingPost/card/Card.jsx
@@ -18,17 +18,36 @@ function Card() {
   return (
     <>
       {hasCard ? (
-        <button className="card">
-          <div className="card--from-group">
-            <div className="card--from-group--title">
-              <img src={mockImg} alt="목업 이미지" />
-              {/* TODO : From 뒤에 이름은 받아서 와야 함 */}
-              <div className="">
-                <span>From.김동훈</span>
+        <button className="card card--flex">
+          <div className="card--container">
+            <div className="card--from-group">
+              <div className="card--from-group--img profile-picture--large">
+                <img src={mockImg} alt="목업 이미지" />
+              </div>
+
+              <div className="text-group">
+                <div className="card--from">
+                  <span className="from">From.</span>
+
+                  <strong className="caller">김동훈</strong>
+                </div>
+
                 {/* 뱃지도 받아서 와야 함 */}
-                <span>동료</span>
+                <div className="badge">
+                  <p className="badge--title">동료</p>
+                </div>
               </div>
             </div>
+
+            <div className="container">
+              <p className="content">
+                코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심
+                또
+                하세요!ddddddddddsdfsjfhsidfjlsdjfksdjlfjksdflajkdjflasjdlfkjsaldkjfkaksdjlkfjlsdjfsaj
+              </p>
+            </div>
+
+            <div className="createdAt">2023.04.21</div>
           </div>
         </button>
       ) : (
