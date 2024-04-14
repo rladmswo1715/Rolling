e25a55ff49590ac4e807f095096207aac642f516
Batrnan <tjdwo2323@naver.com>
2024-04-14 13:07:07 +0900

    Add: 따옴표 수정, 헤더에 z-index값 추가

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -4,14 +4,14 @@ const logoImage = '/assets/logo/logo_rolling.svg';
 
 export default function Header() {
   return (
-    <header className='header__wrap'>
-      <div className='inner__size-ls d__flex--btw header__inner'>
+    <header className="header__wrap">
+      <div className="inner__size-ls d__flex--btw header__inner">
         <Link to={'/'}>
-          <img src={logoImage} alt='롤링' className='header__image__logo' />
+          <img src={logoImage} alt="롤링" className="header__image__logo" />
         </Link>
         <Link to={'/post'}>
-          <div className='header__button-box'>
-            <button className='button__size-h40 button--outlined'>
+          <div className="header__button-box">
+            <button className="button__size-h40 button--outlined">
               롤링 페이퍼 만들기
             </button>
           </div>
