f5516e8366b21a6044637f6c685dd6e86d0091dc
yd170214 <sh20170214@gmail.com>
2024-04-14 14:50:55 +0900

    Docs: (RollingList.jsx) fetch Recipient api
    Recipient는 완료했지만, 그 안에 있는 recentMessage, topReaction은 못했습니다.
    주강사님께 물어보고 수정하겠습니다.

--- src/pages/rollingList/RollingList.jsx
+++ src/pages/rollingList/RollingList.jsx
@@ -1,37 +1,50 @@
 import { Link } from 'react-router-dom';
 import './rollingList.scss';
 import RollingPaper from '../../components/rollingList/RollingPaper';
+import { useState, useEffect } from 'react';
+import { BASE_URL_RECIPIENT } from '../../constants/url';
+// import BASE_URL_MESSAGE from '../../constants/url';
 
 export default function RollingList() {
+  const [recipient, setRecipient] = useState([]);
+  const [recentMessage, setRecentMessage] = useState([]);
+  const [topReaction, setTopReaction] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function getRecipientData(limit = 8, offset = 0) {
+    try {
+      setIsLoading(true);
+      const response = await fetch(
+        `${BASE_URL_RECIPIENT}?limit=${limit}&offset=${offset}`
+      );
+      if (response.ok) {
+        const res = await response.json();
+        setRecipient(res.results);
+        setRecentMessage(res.results.recentMessage);
+        setTopReaction(res.results.topReaction);
+        setIsLoading(false);
+      } else {
+        throw new Error('데이터를 받아오는데 실패했습니다!');
+      }
+    } catch (error) {
+      setError(error);
+    }
+  }
+
+  useEffect(() => {
+    getRecipientData(8, 0);
+  }, []);
+
+  // 아래 주석을 해제해서 데이터를 미리 출력해볼 수 있어요!
+  console.log(recipient);
+
   return (
     <section className="layout__list">
       <div className="inner__size-full inner__body">
-        <div>
-          <div className="RollingList">
-            <div className="RollingList__wrap">
-              <h1>인기 롤링 페이퍼 🔥</h1>
-              <div className="RollingList__wrap--papers">
-                <RollingPaper />
-                <RollingPaper />
-                <RollingPaper />
-                <RollingPaper />
-              </div>
-            </div>
-            <div className="RollingList__wrap">
-              <h1>최근에 만든 롤링 페이퍼⭐️ </h1>
-              <div>
-                <RollingPaper />
-              </div>
-            </div>
-            <div className="button-box">
-              <Link to={'/post'}>
-                <button className="button--fill-primary button__size-h56 main__button-box">
-                  나도 만들어보기
-                </button>
-              </Link>
-            </div>
-          </div>
-        </div>
+        {/* 여기서 작업해주세요 */}
+        롤링페이퍼 리스트 페이지
+        <div>박스</div>
       </div>
     </section>
   );
