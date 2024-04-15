e2c6ee777b13d6347817db64e9dc66e377d10ffa
hoonj170214 <134394435+hoonj170214@users.noreply.github.com>
2024-04-15 18:07:14 +0900

    Revert "[장성훈][RollingList.jsx] 데이터 GET"

--- src/pages/rollingList/RollingList.jsx
+++ src/pages/rollingList/RollingList.jsx
@@ -1,45 +1,37 @@
 import { Link } from 'react-router-dom';
 import './rollingList.scss';
 import RollingPaper from '../../components/rollingList/RollingPaper';
-import { useState, useEffect } from 'react';
-import { BASE_URL_RECIPIENT } from '../../constants/url';
 
 export default function RollingList() {
-  const [recipients, setRecipients] = useState([]);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  async function getRecipientData(limit = 8, offset = 0) {
-    try {
-      setIsLoading(true);
-      const response = await fetch(
-        `${BASE_URL_RECIPIENT}?limit=${limit}&offset=${offset}`
-      );
-      if (response.ok) {
-        const res = await response.json();
-        setRecipients(res.results);
-        setIsLoading(false);
-      } else {
-        throw new Error('데이터를 받아오는데 실패했습니다!');
-      }
-    } catch (error) {
-      setError(error);
-    }
-  }
-
-  useEffect(() => {
-    getRecipientData(10, 0);
-  }, []);
-
-  console.log(recipients);
-
   return (
     <section className="layout__list">
       <div className="inner__size-full inner__body">
-        {/* 여기서 작업해주세요 */}
-        롤링페이퍼 리스트 페이지
-        <div>아래는 fetch된 데이터를 사용하는 예시에요</div>
-        {!isLoading && recipients.map((el) => <p key={el.id}>id는 {el.id}</p>)}
+        <div>
+          <div className="RollingList">
+            <div className="RollingList__wrap">
+              <h1>인기 롤링 페이퍼 🔥</h1>
+              <div className="RollingList__wrap--papers">
+                <RollingPaper />
+                <RollingPaper />
+                <RollingPaper />
+                <RollingPaper />
+              </div>
+            </div>
+            <div className="RollingList__wrap">
+              <h1>최근에 만든 롤링 페이퍼⭐️ </h1>
+              <div>
+                <RollingPaper />
+              </div>
+            </div>
+            <div className="button-box">
+              <Link to={'/post'}>
+                <button className="button--fill-primary button__size-h56 main__button-box">
+                  나도 만들어보기
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/pages/rollingPost/RollingPost.jsx
+++ src/pages/rollingPost/RollingPost.jsx
@@ -1,64 +1,20 @@
 import './rollingPost.scss';
-import { useState, useEffect } from 'react';
 import Card from '../../components/rollingPost/card/Card';
 import Emoji from '../../components/rollingPost/emoji/Emoji';
-import { BASE_URL_RECIPIENT, BASE_URL_MESSAGE } from '../../constants/url';
-import useRecipient from '../../hooks/useRecipient';
 
 export default function RollingPost() {
-  // const [recipients, setRecipients] = useState([]);
-  const [recentMessages, setRecentMessages] = useState([]);
-  const [topReactions, setTopReactions] = useState([]);
+  // const handleCardClick = () => {
+  //   console.log('버튼 클릭!');
+  //   return <Link to={<RollingPostEdit />} />;
+  // };
 
-  // const [error, setError] = useState(null);
-  // const [isLoading, setIsLoading] = useState(false);
-
-  const { recipients, isLoading, error, getRecipientData } = useRecipient();
-  // async function getRecipientData(limit = 8, offset = 0) {
-  //   try {
-  //     setIsLoading(true);
-  //     const response = await fetch(
-  //       `${BASE_URL_RECIPIENT}?limit=${limit}&offset=${offset}`
-  //     );
-  //     if (response.ok) {
-  //       const res = await response.json();
-  //       // setRecipients(res.results); // 성공적
-
-  //       res.results.map((el) => {
-  //         setRecentMessages(el.recentMessages);
-  //         setTopReactions(el.topReaction);
-  //       });
-
-  //       setTopReactions(res.results.topReaction);
-  //       setIsLoading(false);
-  //     } else {
-  //       throw new Error('데이터를 받아오는데 실패했습니다!');
-  //     }
-  //   } catch (error) {
-  //     setError(error);
-  //   }
-  // }
-
-  useEffect(() => {
-    getRecipientData(8, 0);
-  }, []);
-
-  useEffect(() => {
-    console.log(recentMessages);
-  }, [recentMessages]);
-  // 아래 주석을 해제해서 데이터를 미리 출력해볼 수 있어요!
-  console.log(recipients);
   return (
     <section className="layout__post">
       <div className="inner__size-ls inner__body">
         {/* 여기서 작업해주세요 */}
-
+        <span>생성된 롤링페이퍼 페이지 /post/id</span>
         <Card />
         <Emoji />
-        {!isLoading &&
-          recipients.map((el) => (
-            <p key={el.id}>수신자의 백그라운드컬러 {el.backgroundColor}</p>
-          ))}
       </div>
     </section>
   );
