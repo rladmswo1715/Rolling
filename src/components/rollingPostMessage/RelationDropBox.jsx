cd4fe0ebd6b973560703c460216fc4868a81feea
eunjae <rladmswo1715@naver.com>
2024-04-15 19:40:52 +0900

    Fix: DropBox 컴포넌트 Drop down 작업

--- src/components/rollingPostMessage/RelationDropBox.jsx
+++ src/components/rollingPostMessage/RelationDropBox.jsx
@@ -1,4 +1,9 @@
+import './relationDropBox.scss';
 import { useState } from "react";
+
+function dropBoxArrowToggle(element, toggleBoolean) {
+  element.classList.toggle('hide', toggleBoolean);
+}
 
 export default function RelationDropBox() {
   const [seletedRelation, setSelectedRelation] = useState('지인');
@@ -11,15 +16,17 @@ export default function RelationDropBox() {
     };
     setSelectedRelation(e.target.dataset.value);
     setIsDropBoxOpen(false);
+    dropBoxArrowToggle(document.querySelector('#relationDropDown'), false);
   }
 
   function handleOpenDropBox(e) {
     isDropBoxOpen ? setIsDropBoxOpen(false) : setIsDropBoxOpen(true);
+    dropBoxArrowToggle(e.target, !isDropBoxOpen);
   }
 
   return (
     <div className="dropdown">
-      <button type="button" className="dropdown__toggle hide" onClick={handleOpenDropBox}>{seletedRelation}</button>
+      <button type="button" className="dropdown__toggle" id="relationDropDown" onClick={handleOpenDropBox}>{seletedRelation}</button>
       {isDropBoxOpen && 
         <ul className="dropdown__menu" onClick={handleSelectRelation}>
           <li className="item" data-value={"지인"}>지인</li>
