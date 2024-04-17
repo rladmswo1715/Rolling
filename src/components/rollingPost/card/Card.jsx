b8f9b5f4e9427a5f4b3887573f5d842e0141e5ce
yd170214 <sh20170214@gmail.com>
2024-04-17 15:29:20 +0900

    fix: Card 컴포넌트 mock 데이터 제거, api를 통해 받아온 데이터 렌더링

--- src/components/rollingPost/card/Card.jsx
+++ src/components/rollingPost/card/Card.jsx
@@ -1,65 +1,40 @@
 import './card.scss';
 import '../../../styles/components.scss';
-import { Link } from 'react-router-dom';
-import RollingPostEdit from '../../../pages/rollingPostEdit/RollingPostEdit';
-import plusIcon from '/assets/icon/icon_plus.svg';
-import mockImg from './mock_img.png';
-import { useState } from 'react';
+import { formatDate } from '../../../utills/time';
+import Badge from './Badge';
 
-function Card() {
-  const [hasCard, setHasCard] = useState(false);
-
-  const handleCardClick = () => {
-    console.log('버튼 클릭!');
-    setHasCard(true);
-    return <Link to={<RollingPostEdit />} />;
-  };
+export default function Card({
+  sender,
+  content,
+  createdAt,
+  relationship,
+  profileImageURL,
+}) {
+  const formatedDate = formatDate(createdAt);
 
   return (
-    <>
-      {hasCard ? (
-        <button className="card">
-          <div className="card--container">
-            <div className="card--from-group">
-              <div className="card--from-group--img profile-picture--large">
-                <img src={mockImg} alt="목업 이미지" />
-              </div>
-
-              <div className="card--profile-text-group">
-                <div className="card--from">
-                  <span>From.</span>
-                  <strong>김동훈</strong>
-                </div>
-
-                {/* 뱃지도 받아서 와야 함 */}
-                <div className="badge">
-                  <p className="badge--title">동료</p>
-                </div>
-              </div>
-            </div>
-
-            <div className="content">
-              <p className="content--short">
-                코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심
-                또 하세요!
-                가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하
-              </p>
-            </div>
-
-            <div className="createdAt">2023.04.21</div>
+    <button className="card">
+      <div className="card--container">
+        <div className="card--from-group">
+          <div className="card--from-group--img profile-picture--large">
+            <img src={profileImageURL} alt="목업 이미지" />
           </div>
-        </button>
-      ) : (
-        <button className="card" onClick={handleCardClick}>
-          <div className="card--empty">
-            <div className="card--img-container">
-              <img src={plusIcon} alt="추가하기" />
+
+          <div className="card--profile-text-group">
+            <div className="card--from">
+              <span>From.</span>
+              <strong>{sender}</strong>
             </div>
+            <Badge relationship={relationship} />
           </div>
-        </button>
-      )}
-    </>
+        </div>
+
+        <div className="content">
+          <p className="content--short">{content}</p>
+        </div>
+
+        <div className="createdAt">{formatedDate}</div>
+      </div>
+    </button>
   );
 }
-
-export default Card;
