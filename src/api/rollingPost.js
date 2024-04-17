63239e7e8a6c0a3a493b5dda301912b70a118ab2
yd170214 <sh20170214@gmail.com>
2024-04-17 15:21:27 +0900

    feat: rollingPost 데이터 get 훅 제작

--- src/api/rollingPost.js
+++ src/api/rollingPost.js
@@ -1,20 +1,18 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import { BASE_URL_RECIPIENT } from '../constants/url';
 
-export default function useRecipient() {
-  const [recipients, setRecipients] = useState([]);
+export function useGetRecipient() {
+  const [recipient, setRecipients] = useState([]);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  async function getRecipientData(limit = 8, offset = 0) {
+  const getRecipientData = useCallback(async (id) => {
     try {
       setIsLoading(true);
-      const response = await fetch(
-        `${BASE_URL_RECIPIENT}?limit=${limit}&offset=${offset}`
-      );
+      const response = await fetch(`${BASE_URL_RECIPIENT}${id}/`);
       if (response.ok) {
         const res = await response.json();
-        setRecipients(res.results);
+        setRecipients(res);
         setIsLoading(false);
       } else {
         throw new Error('데이터를 받아오는데 실패했습니다!');
@@ -22,7 +20,33 @@ export default function useRecipient() {
     } catch (error) {
       setError(error);
     }
-  }
+  }, []);
 
-  return { recipients, isLoading, error, getRecipientData };
+  return { recipient, isLoading, error, getRecipientData };
+}
+
+export function useGetMessage() {
+  const [message, setMessage] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getMessage = useCallback(async (id, limit = 8, offset = 0) => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(
+        `${BASE_URL_RECIPIENT}${id}/messages/?limit=${limit}&offset=${offset}`
+      );
+      if (response.ok) {
+        const res = await response.json();
+        setMessage(res.results);
+        setIsLoading(false);
+      } else {
+        throw new Error('데이터를 받아오는데 실패했습니다!');
+      }
+    } catch (error) {
+      setError(error);
+    }
+  }, []);
+
+  return { message, isLoading, error, getMessage };
 }
