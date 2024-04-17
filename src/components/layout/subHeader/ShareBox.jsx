a869df3c56e8d94bf708c2f97ca3b533216a1dee
kimmiso <vmffjrm@naver.com>
2024-04-17 22:42:46 +0900

    Feat: kakao 공유하기 연결까지 작업

--- src/components/layout/subHeader/ShareBox.jsx
+++ src/components/layout/subHeader/ShareBox.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./shareBox.scss";
+import kakaoImg from '/assets/image/kakao_share_image.png'
+const { Kakao } = window;
 
 export default function ShareBox() {
   const [isShareOpen, setIsShareOpen] = useState(false);
@@ -7,7 +9,51 @@ export default function ShareBox() {
   const handleDoropDwonOpen = () => {
     setIsShareOpen((prev) => !prev);
   };
+  const [copied, setCopied] = useState(false);
 
+  const handleURLCopy = () => {
+    const currentUrl = window.location.href;
+    navigator.clipboard
+      .writeText(currentUrl)
+      .then(() => {
+        setCopied(true);
+        setTimeout(() => setCopied(false), 5000); // 5초 후에 토스트 메시지 사라짐
+      })
+      .catch((error) => {
+        console.error("클립보드 복사 실패:", error);
+      });
+  };
+
+  // 카카오톡 공유하기 
+  useEffect(()=>{
+    Kakao.cleanup();
+    Kakao.init(import.meta.env.VITE_KAKAO_KEY);
+  },[])
+
+  const handleKaKaoShare = () => {
+    Kakao.Share.sendDefault({
+      objectType: 'feed',
+      content: {
+        title: `Rolling`,
+        description: `${'홍길동'} 롤링을 소개합니다.`,
+        imageUrl:`https://i.pinimg.com/originals/07/ce/fe/07cefe14f9b9e45f0e5f0a42a78f0747.gif`,
+        link: {
+          mobileWebUrl: 'https://developers.kakao.com',
+          webUrl: 'https://developers.kakao.com',
+        },
+      },
+      buttons: [
+        {
+          title: `${'홍길동'} 롤링 페이지로 이동`,
+          link: {
+            mobileWebUrl: 'https://developers.kakao.com',
+            webUrl: 'https://developers.kakao.com',
+          },
+        },
+      ],
+    })
+  }
+  
   return (
     <div className="dropdown share-box--wrap">
       <button
@@ -18,8 +64,8 @@ export default function ShareBox() {
       </button>
       {isShareOpen && (
         <div className="dropdown__menu">
-          <button className="item">카카오톡 공유</button>
-          <button className="item">URL 공유</button>
+          <button className="item" onClick={handleKaKaoShare}>카카오톡 공유</button>
+          <button className="item" onClick={handleURLCopy}>URL 공유</button>
         </div>
       )}
     </div>
