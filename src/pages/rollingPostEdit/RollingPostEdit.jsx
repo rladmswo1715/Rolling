8d3211cb2f980e3f4348ae29f006cde24d9af836
yd170214 <sh20170214@gmail.com>
2024-04-18 16:55:10 +0900

    feat: 롤링페이퍼 삭제 기능

--- src/pages/rollingPostEdit/RollingPostEdit.jsx
+++ src/pages/rollingPostEdit/RollingPostEdit.jsx
@@ -8,16 +8,30 @@ import {
 } from '../../utills/dataToStyle';
 import CardDeleteList from '../../components/rollingPostEdit/CardDeleteList';
 import { useParams } from 'react-router-dom';
-
+import { BASE_URL_RECIPIENT } from '../../constants/url';
 export default function RollingPostEdit() {
   const { recipient, getRecipientData } = useGetRecipient();
-  const id = useParams();
+  const { id } = useParams();
+
+  const hasCard = recipient.length > 0;
 
   useEffect(() => {
     getRecipientData(id);
   }, []);
 
-  console.log(recipient);
+  const handleDeleteClick = async () => {
+    try {
+      const response = await fetch(`${BASE_URL_RECIPIENT}${id}/`, {
+        method: 'DELETE',
+      });
+      if (!response.ok) {
+        throw new Error('롤링 페이퍼를 삭제하는데 실패했습니다');
+      }
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <section>
@@ -28,9 +42,15 @@ export default function RollingPostEdit() {
             : getBackgroundColor(recipient)
         }
       >
-        <div className="button__layout">
-          <button className="button__delete">삭제하기</button>
-        </div>
+        {hasCard ? (
+          <div className="button__layout">
+            <button className="button__delete" onClick={handleDeleteClick}>
+              삭제하기
+            </button>
+          </div>
+        ) : (
+          ''
+        )}
         <div className="cards__layout">
           <CardDeleteList />
         </div>
