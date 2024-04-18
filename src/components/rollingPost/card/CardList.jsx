31e17218a9c08451f2b89d1b1cf4aca26a2d7f03
yd170214 <sh20170214@gmail.com>
2024-04-18 16:50:43 +0900

    fix: useNavigate 삭제

--- src/components/rollingPost/card/CardList.jsx
+++ src/components/rollingPost/card/CardList.jsx
@@ -3,22 +3,16 @@ import { useGetMessage } from '../../../api/rollingPost';
 import './card.scss';
 import EmptyCard from './EmptyCard';
 import Card from './Card';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 export default function CardList() {
   const { message, getMessage } = useGetMessage();
-  const navigate = useNavigate();
-  const id = useParams();
-
-  const handleEmptyCardClick = () => {
-    navigate(`/post/${id}/message`);
-  };
+  const hasCard = message.length > 0;
+  const { id } = useParams();
 
   useEffect(() => {
     getMessage(id, 10, 0);
   }, []);
-
-  const hasCard = message.length > 0;
 
   return hasCard ? (
     message.map((el) => {
@@ -34,6 +28,6 @@ export default function CardList() {
       );
     })
   ) : (
-    <EmptyCard onClick={handleEmptyCardClick} />
+    <EmptyCard />
   );
 }
