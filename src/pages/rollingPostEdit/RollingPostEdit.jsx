e59c8fef2208338018949de187645421b440e600
yd170214 <sh20170214@gmail.com>
2024-04-18 11:28:10 +0900

    feat: react-router-dom 페이지 라우팅

--- src/pages/rollingPostEdit/RollingPostEdit.jsx
+++ src/pages/rollingPostEdit/RollingPostEdit.jsx
@@ -1,15 +1,40 @@
 import './rollingPostEdit.scss';
+import { useEffect } from 'react';
+import { useGetRecipient } from '../../api/rollingPost';
+import {
+  hasBackgroundImage,
+  getBackgroundImage,
+  getBackgroundColor,
+} from '../../utills/dataToStyle';
+import CardDeleteList from '../../components/rollingPostEdit/CardDeleteList';
+import { useParams } from 'react-router-dom';
 
 export default function RollingPostEdit() {
+  const { recipient, getRecipientData } = useGetRecipient();
+  const id = useParams();
+
+  useEffect(() => {
+    getRecipientData(id);
+  }, []);
+
+  console.log(recipient);
+
   return (
-    <section className="layout__edit">
-      <div className="inner__size-ls inner__body">
-        {/* 여기서 작업해주세요 */}
-        생성된 롤링페이퍼 수정 페이지
-        <div>
-          박스
+    <section>
+      <div
+        className={
+          hasBackgroundImage(recipient)
+            ? getBackgroundImage(recipient)
+            : getBackgroundColor(recipient)
+        }
+      >
+        <div className="button__layout">
+          <button className="button__delete">삭제하기</button>
+        </div>
+        <div className="cards__layout">
+          <CardDeleteList />
         </div>
       </div>
     </section>
-  )
+  );
 }
