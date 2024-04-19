8d0b74aec27746a81fde52ad79dc3b46b67d967d
yd170214 <sh20170214@gmail.com>
2024-04-19 17:13:58 +0900

    feat: post 페이지 modal 기능 추가

--- src/components/rollingPost/card/CardList.jsx
+++ src/components/rollingPost/card/CardList.jsx
@@ -1,22 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useGetMessage } from '../../../api/rollingPost';
 import './card.scss';
 import EmptyCard from './EmptyCard';
 import Card from './Card';
 import { useParams } from 'react-router-dom';
+import Modal from '../../../components/modal/Modal';
 
 export default function CardList() {
   const { message, getMessage } = useGetMessage();
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const hasCard = message.length > 0;
   const { id } = useParams();
+  const [clickedMessageId, setClickedMessageId] = useState();
+
+  const handleCardClick = (messageId) => {
+    setIsModalOpen(true);
+    for (let a = 0; a < message.length; a++) {
+      if (message[a].id === messageId) {
+        setClickedMessageId(a);
+      }
+    }
+  };
+
+  // console.log(messageValue);
 
   useEffect(() => {
     getMessage(id, 10, 0);
   }, []);
 
-  useEffect(() => {
-    console.log(message);
-  }, [message]);
+  // useEffect(() => {
+  //   console.log(message);
+  // }, [message]);
 
   return (
     <>
@@ -33,10 +47,19 @@ export default function CardList() {
                 relationship={el.relationship}
                 profileImageURL={el.profileImageURL}
                 font={el.font}
+                cardClick={() => {
+                  handleCardClick(el.id);
+                }}
               />
             );
           })
         : ''}
+      {isModalOpen && (
+        <Modal
+          cardData={message[clickedMessageId]}
+          isModalOpen={setIsModalOpen}
+        />
+      )}
     </>
   );
 }
