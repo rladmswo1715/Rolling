add0334c8305d26a2c4bfbb472dc0b35612bdc75
yd170214 <sh20170214@gmail.com>
2024-04-22 18:00:01 +0900

    fix: 모달 생성 기능

---
+++ src/api/rollingPostEdit.js
@@ -0,0 +1,13 @@
+export const deleteData = async (url) => {
+  try {
+    const response = await fetch(url, {
+      method: 'DELETE',
+    });
+    if (!response.ok) {
+      throw new Error('롤링 페이퍼를 삭제하는데 실패했습니다');
+    }
+    // setResponse(response);
+  } catch (error) {
+    console.error(error);
+  }
+};
