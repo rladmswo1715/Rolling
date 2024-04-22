af132e628a23e7b42855fc870bb34db710b4a8a8
Batrnan <tjdwo2323@naver.com>
2024-04-22 18:20:08 +0900

    Feat: 슬라이드 끝났을 때 데이터 불러오기

--- src/components/rollingList/PopularPapers.jsx
+++ src/components/rollingList/PopularPapers.jsx
@@ -1,58 +1,100 @@
 import RollingPaper from './RollingPaper';
 import './popularPapers.scss';
-
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/scss';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import './swiperStyles.scss';
 import { Pagination, Navigation, Autoplay } from 'swiper/modules';
-import { Link } from 'react-router-dom';
+import getRollingList from '../../api/rollingList';
 
-export default function PopularPapers({ recipients }) {
-  if (!recipients) return null;
+export default function PopularPapers() {
   const [swiperRef, setSwiperRef] = useState(null);
-  const popularRecipients = recipients.slice();
+  const [recipients, setRecipients] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [nextUri, setNextUri] = useState(null);
 
-  // 이모지 많은 순으로 정렬
-  popularRecipients.sort((a, b) => b.reactionCount - a.reactionCount);
+  const handleLoad = async (options) => {
+    try {
+      setIsLoading(true);
+      setError(null);
+      const { results, next } = await getRollingList(options);
+      setNextUri(next);
+      if (options.offset === 0) {
+        setRecipients(results);
+      } else {
+        setRecipients((prevRecipients) => [...prevRecipients, ...results]);
+      }
+      setOffset(options.offset + results.length);
+    } catch (error) {
+      setError(error);
+      return;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleLoadMore = () => {
+    handleLoad({ sort: 'like', limit: 8, offset });
+  };
+
+  const handleSlideChangeEnd = (swiper) => {
+    // Swiper 객체를 이용하여 현재 슬라이드의 인덱스 확인
+    setCurrentIndex(swiper.realIndex);
+    // 슬라이드가 끝났을 경우 API 호출 실행
+    if (swiper.isEnd && nextUri) {
+      handleLoadMore();
+    }
+  };
+
+  useEffect(() => {
+    handleLoad({ sort: 'like', limit: 8, offset: 0 });
+  }, []);
 
   return (
     <div className="PopularPapers">
       <h1 className="PopularPapers--title">인기 롤링 페이퍼 🔥</h1>
       <div className="PopularPapers--papers">
-        <Swiper
-          onSwiper={setSwiperRef}
-          slidesPerView={'auto'}
-          centeredSlides={false}
-          spaceBetween={30}
-          pagination={{
-            type: 'fraction',
-          }}
-          navigation={true}
-          modules={[Pagination, Navigation]}
-          className="mySwiper"
-          breakpoints={{
-            1100: {
-              slidesPerView: 4,
-            },
-          }}
-          autoplay={{
-            delay: 3000,
-            disableOnInteraction: false,
-          }}
-        >
-          {popularRecipients.map((popularRecipient) => {
-            return (
-              <SwiperSlide key={popularRecipient.id}>
-                <Link to={`/post/${popularRecipient.id}`}>
-                  <RollingPaper recipient={popularRecipient} />
-                </Link>
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
+        {!isLoading && (
+          <Swiper
+            onSwiper={setSwiperRef}
+            slidesPerView={'auto'}
+            centeredSlides={false}
+            spaceBetween={30}
+            pagination={{
+              type: 'fraction',
+            }}
+            navigation={true}
+            modules={[Pagination, Navigation]}
+            className="mySwiper"
+            breakpoints={{
+              1250: {
+                slidesPerView: 4,
+              },
+            }}
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+            }}
+            onSlideChangeTransitionEnd={handleSlideChangeEnd}
+            initialSlide={currentIndex}
+          >
+            {recipients.map((recipient, index) => {
+              return (
+                <SwiperSlide key={`${recipient.id}-${index}`}>
+                  <Link to={`/post/${recipient.id}`}>
+                    <RollingPaper recipient={recipient} />
+                  </Link>
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        )}
       </div>
     </div>
   );
--- src/pages/rollingList/RollingList.jsx
+++ src/pages/rollingList/RollingList.jsx
@@ -1,43 +1,15 @@
 import { Link } from 'react-router-dom';
 import './rollingList.scss';
-import { useState, useEffect } from 'react';
-import { BASE_URL_RECIPIENT } from '../../constants/url';
 import PopularPapers from '../../components/rollingList/PopularPapers';
 import RecentPapers from '../../components/rollingList/RecentPapers';
 
 export default function RollingList() {
-  const [recipients, setRecipients] = useState([]);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  async function getRecipientData(limit = 8, offset = 0) {
-    try {
-      setIsLoading(true);
-      const response = await fetch(
-        `${BASE_URL_RECIPIENT}?limit=${limit}&offset=${offset}`
-      );
-      if (response.ok) {
-        const res = await response.json();
-        setRecipients(res.results);
-        setIsLoading(false);
-      } else {
-        throw new Error('데이터를 받아오는데 실패했습니다!');
-      }
-    } catch (error) {
-      setError(error);
-    }
-  }
-
-  useEffect(() => {
-    getRecipientData(20, 0);
-  }, []);
-
   return (
     <section className="layout__list">
       <div className="inner__size-full inner__body">
         <div className="RollingList">
-          <PopularPapers recipients={recipients} />
-          <RecentPapers recipients={recipients} />
+          <PopularPapers />
+          <RecentPapers />
           <div className="button-box">
             <Link to={'/post'}>
               <button className="button--fill-primary button__size-h56 main__button-box">
