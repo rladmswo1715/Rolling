7a1feeb088633494a68500e87ab4449a8f562895
yd170214 <sh20170214@gmail.com>
2024-04-23 15:15:12 +0900

    chore: console.log 지움, 메인 test.jsx 지움

--- src/components/rollingList/PopularPapers.jsx
+++ src/components/rollingList/PopularPapers.jsx
@@ -13,10 +13,53 @@ import { Link } from 'react-router-dom';
 export default function PopularPapers({ recipients }) {
   if (!recipients) return null;
   const [swiperRef, setSwiperRef] = useState(null);
+
+  const [recipients, setRecipients] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [nextUri, setNextUri] = useState(null);
+
   const popularRecipients = recipients.slice();
 
   // 이모지 많은 순으로 정렬
   popularRecipients.sort((a, b) => b.reactionCount - a.reactionCount);
+
+  const handleLoad = async (options, nextUri) => {
+    try {
+      setIsLoading(true);
+      setError(null);
+      const { results, next } = await getRollingList(options, nextUri);
+      setNextUri(next);
+      if (options.offset === 0) {
+        setRecipients(results);
+      } else {
+        setRecipients((prevRecipients) => [...prevRecipients, ...results]);
+      }
+    } catch (error) {
+      setError(error);
+      return;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleLoadMore = () => {
+    handleLoad({}, nextUri);
+  };
+
+  const handleSlideChangeEnd = (swiper) => {
+    // Swiper 객체를 이용하여 현재 슬라이드의 인덱스 확인
+    setCurrentIndex(swiper.realIndex);
+    // 슬라이드가 끝났을 경우 API 호출 실행
+    if (swiper.isEnd && nextUri) {
+      handleLoadMore();
+    }
+  };
+
+  useEffect(() => {
+    handleLoad({ sort: 'like', limit: 8, offset: 0 });
+  }, []);
 
   return (
     <div className="PopularPapers">
